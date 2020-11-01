faef2f17faa612ef281009779a27e6b8a9030733
theol93 <dbtheoldb@gmail.com>
2020-11-01 14:55:06 +0200

    starting add Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./app/App";
+import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const middleware = [ thunk ];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+)
+
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
